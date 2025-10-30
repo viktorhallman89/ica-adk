dd8749c3de79b96a1f7b158c0e79d47b51df34ac
jt151077 <jt151077@gmail.com>
2025-10-30 07:55:11 +0000

    new orchestrating agent

--- ica_agent/__init__.py
+++ ica_agent/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import seq, tools
+from . import agent, tools
--- ica_agent/agent.py
+++ ica_agent/agent.py
@@ -12,21 +12,81 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from google.adk.agents import Agent
 
-# Import the tools
-from .tools import generate_image_data
-from google.adk.tools.load_artifacts_tool import load_artifacts_tool
+from google.adk.agents import Agent, SequentialAgent, LlmAgent
+from toolbox_core import ToolboxSyncClient
 
+toolbox = ToolboxSyncClient("https://toolbox-489070644303.us-central1.run.app/")
+
+# Load all the tools
+tools = toolbox.load_toolset('my_bq_toolset')
+
+product_retrieval_agent = LlmAgent(
+    name="product_retrieval_agent",
+    model="gemini-2.5-flash",
+    description=(
+        "Agent to order information from the tool, based information passed by the user. The user should prompted to provide order_id and product_name."
+    ),
+    instruction=(
+        "You are a helpful agent who will retrieve order_id, product_id, product_name and the quantity. Ask the user to provide both order_id and product_name that is missing from the delivery. Use the tools to answer the question, by passing in the strict order, the order_id and the product_name. Display all information retrieved by the tool as a JSON object. Pass the product_name to the next agent."
+    ),
+    tools=tools,
+    output_key="product_name"
+)
+
+# product_retrieval_agent = LlmAgent(
+#     name="product_retrieval_agent",
+#     model="gemini-2.5-flash-lite",
+#     instruction="""
+#      for now just retrieve that product name is ketchup 
+#     """,
+#     description="Retrieve missing product from customer order",
+#     output_key="product_name"
+# )
+
+
+
+picture_analysis_agent = LlmAgent(
+    name="picture_analysis_agent",
+    model="gemini-2.5-flash-lite",
+    instruction="""for now identify that the product is missing from picture so answer is the product is missing confirmed for refund and pass missing product_name
+  """,
+    description="Check if image contains missing product based on product_name",
+    output_key="Answer"
+)
+
+refund_agent = LlmAgent(
+    name="refund_agent",
+    model="gemini-2.5-flash-lite",
+    instruction="""
+1.  Identify Missing Item Potential Cost based on missing product_name. This value will be the basis for the refund voucher.
+2.  Generate Unique Voucher Code: Create a unique, alphanumeric refund voucher code. The code should be between 10 and 15 characters long, including a "REFUND-" prefix, and incorporate a mix of uppercase letters and numbers (e.g., `REFUND-XYZ123ABC`). Ensure the code is distinct each time.
+3.  Set Voucher Value: The monetary value of the generated voucher must be *exactly equal* to the missing product cost identified in step 1.
+4.  Determine Currency: Assume the currency is SEK unless specified otherwise in the user's request. If a different currency is explicitly mentioned with the cost, use that currency.
+5.  Create Description: Provide a concise description for the voucher, such as "Refund voucher for missing item."
+8.  Format Output: Present all the generated voucher information in a structured JSON object.
+
+Output Format:
+Your response must be a JSON object with the following keys
+ """,
+    description="Based on missing item you are generating a refund voucher for the user",
+    output_key="voucher",
+)
+
+complain_processing_agent = SequentialAgent(
+    name="complain_processing_agent",
+    sub_agents=[product_retrieval_agent, picture_analysis_agent, refund_agent],
+    description="This agent is designed to automate and verify customer complaints regarding missing products in ICA Supermarket online grocery orders, aiming to resolve disputes quickly and accurately using visual evidence",
+)
 
 
 root_agent = Agent(
-    model="gemini-2.0-flash-001",
+    model="gemini-2.5-flash",
     name='agents',
     instruction="""
-      Ask the user for a year in arabic format.
-      You can only accept a year up until the current date's year.
-      You can only accept integers, and should refuse any inputs such as text or media.
+      You are an expert customer service agant. Users will contact you if they have receievd a delivery where an item is missing.
+      Welcome the user and ask for an order_id and a the name of the product that was missing from the delivery.
+      You should 
       When the user as provided you with a valid year:
       1. You should respond with 1 facts from the year provided by the user
       2. Based on this fact from the previous step, call the `generate_image_data` and display the image_artifact in the response.
